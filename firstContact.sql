1d0eea58a0d3867811a58c14495263acc15b0f5f
dlau1 <43829476+dlau1@users.noreply.github.com>
2019-05-23 15:38:29 -0400

    Add files via upload

---
+++ firstContact.sql
@@ -0,0 +1,61 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema Contact Manger
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `Contact Manger` ;
+
+-- -----------------------------------------------------
+-- Schema Contact Manger
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Contact Manger` DEFAULT CHARACTER SET utf8 ;
+USE `Contact Manger` ;
+
+-- -----------------------------------------------------
+-- Table `Contact Manger`.`User Information`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Contact Manger`.`User Information` (
+  `First Name` VARCHAR(45) NOT NULL,
+  `Last Name` VARCHAR(45) NOT NULL,
+  `Username` VARCHAR(45) NOT NULL,
+  `Password` VARCHAR(45) NOT NULL,
+  `Email Adress` VARCHAR(45) NOT NULL,
+  `user id` INT UNSIGNED NOT NULL,
+  UNIQUE INDEX `Username_UNIQUE` (`Username` ASC),
+  UNIQUE INDEX `Email Adress_UNIQUE` (`Email Adress` ASC),
+  PRIMARY KEY (`user id`),
+  UNIQUE INDEX `user id_UNIQUE` (`user id` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Contact Manger`.`Contact Information`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Contact Manger`.`Contact Information` (
+  `First Name` VARCHAR(45) NULL,
+  `Last Name` VARCHAR(45) NULL,
+  `Home Phone` VARCHAR(45) NULL,
+  `Work Phone` VARCHAR(45) NULL,
+  `Cell Phone` VARCHAR(45) NULL,
+  `Personal Email` VARCHAR(45) NULL,
+  `Work Email` VARCHAR(45) NULL,
+  `Address` VARCHAR(100) NULL,
+  `CID` INT NOT NULL AUTO_INCREMENT,
+  `UID` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`CID`),
+  INDEX `fk_Contact Information_User Information_idx` (`UID` ASC),
+  CONSTRAINT `fk_Contact Information_User Information`
+    FOREIGN KEY (`UID`)
+    REFERENCES `Contact Manger`.`User Information` (`user id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
