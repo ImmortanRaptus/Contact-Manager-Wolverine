875cb96b911e5ed949e32332ab75bc10bfc67b54
Phillip Cheng <cheng_philll@knights.ucf.edu>
2019-05-23 20:36:41 -0400

    Add files via upload
    Just testing git, have a fun weekend

---
+++ Config.php
@@ -0,0 +1,20 @@
+<?php
+
+    session_start();
+
+    //so I dont have to go into each and edit multiple times
+    $host = "localhost"; //where is the sql hosted prob local
+    $user = "root"; //name we login for sql
+    $password = ""; //password for sql
+    $databaseName = "ContactManager"; //name of db
+
+    //establish connection with sql database
+    $conn = mysqli_connect($host, $user, $password, $databaseName);
+
+    //check connection
+    if (!$conn)
+    {
+        die("Connection to SQL failed: " . mysqli_connect_error() );
+    }
+
+?>
