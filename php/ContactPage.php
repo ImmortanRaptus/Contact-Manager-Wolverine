2f2e895a8eee1673e41c7213aeee0cf5ae1df242
ImmortanRaptus <49250280+ImmortanRaptus@users.noreply.github.com>
2019-06-01 18:21:29 -0400

    Fixed w/ namechange

---
+++ php/ContactPage.php
@@ -0,0 +1,40 @@
+<?php
+
+//WIP
+//show all contacts
+require "Config.php";
+
+$userId =  $_SESSION['userid'];
+$sql="SELECT * FROM `Contact Information` WHERE `UID` = '" .$userId. "'";
+
+//using cookie method
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+{
+    $rows[] = $row;
+}
+
+echo json_encode($rows);
+
+
+
+//helper funcitons
+function getRequestInfo() {
+    return json_decode(file_get_contents('php://input'), true);
+}
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+function returnWithError( $error ) {
+    $retValue='{"user_id":0, "username":"", "password_hash":"", "Error": "'.$error.'"}';
+    return sendResultInfoAsJson($retValue);
+}
+function returnWithInfo( $id, $username, $password_hash )
+{
+    $retValue = '{"user_id":' . $id . ',"username":"' . $username . '","password_hash":"' . $password_hash . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+?>
