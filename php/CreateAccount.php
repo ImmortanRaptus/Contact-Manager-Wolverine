8635eb0913f023e8678b792d33ef032516e08b55
Phillip Cheng <cheng_philll@knights.ucf.edu>
2019-06-01 18:03:58 -0400

    Update CreateAccount.php
    Update from Live

--- php/CreateAccount.php
+++ php/CreateAccount.php
@@ -1,68 +1,71 @@
 <?php
-    //WORK IN PROGRESS
-    //include config file but change to require
-    require "Config.php";
-    $inData = getRequestInfo();
+//WORK IN PROGRESS
+//include config file but change to require
+require "Config.php";
+$inData = getRequestInfo();
 
-    $username=$_POST["User"];
-    $password=$_POST["Pass"];
-    $email=$_POST["Email"];
-    //is the user name in the databse already
-    $sql = "SELECT * FROM `User Information` where `Username` = ?";
-    $usernameTaken = $conn->prepare($sql);
-    $usernameTaken->bind_param("s", $username);
-    $usernameTaken->execute();
-    $result = $usernameTaken->get_result();
-    $nameChecker = $result->fetch_all();
-    //should be empty if it isnt
-    if (count($nameChecker) > 0)
-    {
-        //send error and maybe not close
-        returnWithError();
-        $conn->close();
-        return;
-    }
+$username=$_POST["User"];
+$password=$_POST["Pass"];
+$email=$_POST["Email"];
 
-    //ok time to add the new user
-    $sql = "INSERT INTO `User Information` (`Username`, `Password`, `Email Address`) VALUES (? , ? , ?)";
-    $addNewUser=$conn->prepare($sql);
-    $addNewUser->bind_param("sss", $username, $password, $email);
-    $addNewUser->execute();
-
-    //NEEDS TO UPDATE TO CONTACY PAGE
-    header('Location: http://www.ProjectWolverine.net/');
-    
-
-    //maybe don't need to close, need to test
+//is the user name in the databse already
+$sql = "SELECT * FROM `User Information` where `Username` = ?";
+$usernameTaken = $conn->prepare($sql);
+$usernameTaken->bind_param("s", $username);
+$usernameTaken->execute();
+$result = $usernameTaken->get_result();
+$nameChecker = $result->fetch_all();
+//should be empty if it isnt
+if (count($nameChecker) > 0)
+{
+    //send error and maybe not close
+    returnWithError();
     $conn->close();
-	return;
+    return;
+}
+
+//ok time to add the new user
+//first HASH THE PASSWORD
+$password = password_hash($password, PASSWORD_BCRYPT);
+$sql = "INSERT INTO `User Information` (`Username`, `Password`, `Email Address`) VALUES (? , ? , ?)";
+$addNewUser=$conn->prepare($sql);
+$addNewUser->bind_param("sss", $username, $password, $email);
+$addNewUser->execute();
+
+
+header('Location: http://www.ProjectWolverine.net/');
+
+
+//maybe don't need to close, need to test
+$conn->close();
+return;
 
 
 
 
 
-    //helper funcitons
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
+//helper funcitons
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-    function sendResultInfoAsJson( $obj )
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
-    
-    function returnWithError( $err )
-    {
-        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-        sendResultInfoAsJson( $retValue );
-    }
-    
-    function returnWithInfo( $firstName, $lastName, $id )
-    {
-        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-        sendResultInfoAsJson( $retValue );
-    }
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+function returnWithInfo( $firstName, $lastName, $id )
+{
+    $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+    sendResultInfoAsJson( $retValue );
+}
 
 ?>
