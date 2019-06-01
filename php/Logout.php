5d0ce3957be3ae0e1d8ab9df5094d57316c4035f
ImmortanRaptus <49250280+ImmortanRaptus@users.noreply.github.com>
2019-06-01 16:19:05 -0400

    Fix URI's

--- php/Logout.php
+++ php/Logout.php
@@ -6,7 +6,7 @@
     // Check user login or not
     if(!isset($_SESSION['userid']))
     {
-        header('Location: index.php');
+        header('Location: ../index.html');
     }
     // logout
     if(isset($_POST['logout_button']))
@@ -16,6 +16,6 @@
         $days = 30;
         setcookie ("rememberme","", time() - ($days * 86400 ));
         //go where after logout
-        header('Location: index.php');
+        header('Location: ../index.html');
     }
 ?>
