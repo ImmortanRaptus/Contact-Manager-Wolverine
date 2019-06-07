5be3b944486818bf2c1534a7e5ab90b5fa353cfe
Phillip Cheng <cheng_philll@knights.ucf.edu>
2019-06-06 20:09:06 -0400

    Update ContactSearch.php

--- php/ContactSearch.php
+++ php/ContactSearch.php
@@ -1,23 +1,33 @@
 <?php
 
-//WIP
 //show all contacts
 include "Config.php";
 
 //NEEDS MORE FIELDS, probably
 //GOTTA FILL OUT THE FIELDS
-//check for backticks (`) and single quotes (')
-$sql="SELECT * FROM `contact information` WHERE `UID` = ?" . "AND ((`First Name` LIKE ?) OR (`Last Name` LIKE ?))";
-$userId = $_SESSION['userid'];
-$searchData = $_POST['Search'];
+//user ID needs to be SESSION 
+
+
+// reset to $_SESSION['userid']  suuuper important
+//  $_POST['userid'];
+
+
+//FOR TESTING
+//$userid = mysqli_real_escape_string($conn,$_POST['userid']);
+
+$userid = $_SESSION['userid'];//user ID
+$searchData = mysqli_real_escape_string($conn,$_POST['Search']); //Search form input
+//THIS IS WHERE WE WILL ADD MORE FIELDS AS NECESSARY
+$sql="SELECT * FROM `Contact Information` WHERE `UID` = '" .$userid. "' AND ((`First Name` LIKE '" .$searchData. "') OR (`Last Name` LIKE '" .$searchData. "'))";
 
 //using cookie method
 //search for first and last name but we can do whatever
-$prepared=$conn->prepare($sql);
-$prepared->bind_param("sss", $userId, $searchData, $searchData);
-$rows = array();
-while($row = mysqli_fetch_array($sql))
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 {
     $rows[] = $row;
 }
+
 echo json_encode($rows);
+
+?>
