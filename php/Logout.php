646e26edd119c25eeae82319badf75bc57dde032
Phillip Cheng <cheng_philll@knights.ucf.edu>
2019-06-06 20:19:42 -0400

    Update Logout.php

--- php/Logout.php
+++ php/Logout.php
@@ -1,21 +1,12 @@
 <?php
 
-    //WIP
-    include "Config.php";
-
-    // Check user login or not
-    if(!isset($_SESSION['userid']))
-    {
-        header('Location: index.php');
-    }
-    // logout
-    if(isset($_POST['logout_button']))
-    {
+	require "Config.php";
+	//nuke it all
         session_destroy();
         //remove cookie
         $days = 30;
         setcookie ("rememberme","", time() - ($days * 86400 ));
         //go where after logout
-        header('Location: index.php');
-    }
+        header('Location: ../index.html');
+ 
 ?>
